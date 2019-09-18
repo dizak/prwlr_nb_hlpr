e7ec4842a72c9b4e1a7cbdb91d46b3ae9ac901b5
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2019-09-18 11:03:08 +0200

    Initial commit

---
+++ prwlr_nb_hlpr/__init__.py
@@ -0,0 +1,16 @@
+"""
+This package provides helper functions and decorators for the jupyter notebooks
+used for the G148 implementation.
+"""
+
+
+__version__ = (0, 0, 0)
+__author__ = 'Dariusz Izak'
+
+
+__all__ = [
+    'objects',
+    'pdutils',
+    'plotting',
+    'stats',
+]
---
+++ tests/tests.py
@@ -0,0 +1,18 @@
+"""
+This module tests the functionalities of the other modules included in this 
+package.
+"""
+
+
+from unittest import TestCase
+
+
+class ObjectsTests(TestCase):
+    """
+    Tests of the custom objects functionalities from the <objects> submodule
+    """
+    def setUp(self):
+        """
+        Set up the environment for the <objects> submodule tests
+        """
+        from prwlr_nb_hlpr import objects
